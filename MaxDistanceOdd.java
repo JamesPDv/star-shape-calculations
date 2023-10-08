be029015974045f58b564c06edd4c994bcb4c358
JamesPDv <146348057+JamesPDv@users.noreply.github.com>
2023-10-08 15:24:30 -0400

    Add files via upload

---
+++ MaxDistanceOdd.java
@@ -0,0 +1,31 @@
+public class MaxDistanceOdd
+{
+    public static void main(String[] args)
+    {
+        int n = 1;                                  //Initializes n as 1.
+        double maxDistance = 1 * Math.sqrt(2) / 2;  //The distance of the furthest point from the origin when n = 1.
+        double numerator = 1 * Math.sqrt(2);        //The numerator of maxDistance.
+        double denominator = 2;                     //The denominator of maxDistance.
+        double numeratorDiff = 1 * Math.sqrt(2);    //The amount the numerator changes by when n increases by 2.
+        int denominatorDiff = 2;                    //The amount the denominator changes by when n increases by 2.
+        double temp;                                //A temporary variable for storing values.
+        double change;                      //The distance between maxDistance and the previous value of maxDistance.
+
+        /*
+        To conserve processing power, this program is only run until n is greater than 100000, which is far beyond when
+        the value of change noticeably converges. n is incremented by 1, but this actually represents 2 increments of n.
+        numeratorDiff and denominatorDiff are accordingly shifted to account for this.
+        */
+        while(n <= 10000)
+        {
+            numeratorDiff += 6 * Math.sqrt(2);      //Increases the amount by which the numerator increases.
+            temp = maxDistance;                     //Assigns the current maxDistance to temp.
+            numerator += numeratorDiff;             //Increases the numerator by numeratorDiff.
+            denominator += denominatorDiff;         //Increases the denominator by denominatorDiff.
+            maxDistance = numerator / denominator;  //Assigns the numerator divided by the denominator to maxDistance.
+            change = maxDistance - temp;            //Calculates the distance between maxDistance and temp.
+            System.out.println(change);             //Prints change.
+            n++;                                    //Increases n by 1, representing two increments of n.
+        }
+    }
+}
